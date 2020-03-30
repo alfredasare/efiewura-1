df3a37c44c91dec78f61d64c3596e5fab2c637e6
Nodenkey <Nodenkey@gmail.com>
2020-03-30 22:39:29 +0000

    added blur

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -8,15 +8,28 @@ import Navbar from "../../components/navbar/navbar.component";
 const ContactPage = () => {
 
     const [isOpen, setIsOpen] = useState(false);
-    const [alert, setAlert] = useState({heading: '', para1: '', para2: '', para3: '', para4: '', para5: '',
+    const [alert, setAlert] = useState({
+        heading: '', para1: '', para2: '', para3: '', para4: '', para5: '',
         para6: '', para7: '',
     });
 
     const showModal = (event) => {
-        if(event.target.id === 'phone'){
-            setAlert({...alert, heading: 'Call Efiewura', para1: '+233 233 233 233', para2: '+233 233 233 234',para3: '+233 233 233 235'});
-        }else if(event.target.id === 'location'){
-            setAlert({...alert, heading: 'Our Offices are located at', para1: 'Enter street name', para2: 'Add more of the address',para3: 'Final part of the address'});
+        if (event.target.id === 'phone') {
+            setAlert({
+                ...alert,
+                heading: 'Call Efiewura',
+                para1: '+233 233 233 233',
+                para2: '+233 233 233 234',
+                para3: '+233 233 233 235'
+            });
+        } else if (event.target.id === 'location') {
+            setAlert({
+                ...alert,
+                heading: 'Our Offices are located at',
+                para1: 'Enter street name',
+                para2: 'Add more of the address',
+                para3: 'Final part of the address'
+            });
         }
         setIsOpen(true);
     };
@@ -29,26 +42,30 @@ const ContactPage = () => {
         <>
             <Navbar/>
             <div className='main-body'>
-                <div className='contact-container'>
-                    <h1 className='main-header'>Reach EFIEWURA</h1>
-                    <div className='line animated slideInLeft'/>
-                    <div className='icon-container'>
-                        <a href="mailto:efiewura18@gmail.com" target="_blank"><i className='fa fa-envelope font-style animated bounceIn'/></a>
-                        <i className='fa fa-phone font-style phone animated bounceIn delay-1s' onClick={showModal} id='phone'/>
-                        <a href="#"><i className='fa fa-facebook font-style fb animated bounceIn delay-2s'/></a>
-                        <a href="#"><i className='fa fa-instagram font-style instagram animated bounceIn delay-3s'/></a>
-                        <a href="#"><i className='fa fa-twitter font-style twitter animated bounceIn delay-4s'/></a>
-                        <i className='fa fa-map-marker font-style animated bounceIn delay-5s' onClick={showModal} id='location'/>
-                    </div>
-                    <div className='line second-line animated slideInRight'/>
-                    <Modal show={isOpen} onHide={hideModal} size="md"
-                           aria-labelledby="contained-modal-title-vcenter"
-                           >
-                        <Modal.Header closeButton>
-                            <Modal.Title id="contained-modal-title-vcenter" >
-                                {alert.heading}
-                            </Modal.Title>
-                        </Modal.Header>
+            </div>
+            <div className='contact-container'>
+                <h1 className='main-header'>Reach EFIEWURA</h1>
+                <div className='line animated slideInLeft'/>
+                <div className='icon-container'>
+                    <a href="mailto:efiewura18@gmail.com" target="_blank"><i
+                        className='fa fa-envelope font-style animated bounceIn'/></a>
+                    <i className='fa fa-phone font-style phone animated bounceIn delay-1s' onClick={showModal}
+                       id='phone'/>
+                    <a href="#"><i className='fa fa-facebook font-style fb animated bounceIn delay-2s'/></a>
+                    <a href="#"><i className='fa fa-instagram font-style instagram animated bounceIn delay-3s'/></a>
+                    <a href="#"><i className='fa fa-twitter font-style twitter animated bounceIn delay-4s'/></a>
+                    <i className='fa fa-map-marker font-style animated bounceIn delay-5s' onClick={showModal}
+                       id='location'/>
+                </div>
+                <div className='line second-line animated slideInRight'/>
+                <Modal show={isOpen} onHide={hideModal} size="md"
+                       aria-labelledby="contained-modal-title-vcenter"
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            {alert.heading}
+                        </Modal.Title>
+                    </Modal.Header>
                     <Modal.Body>
                         <p>
                             {alert.para1}
@@ -66,11 +83,10 @@ const ContactPage = () => {
                             {alert.para5}
                         </p>
                     </Modal.Body>
-                        <Modal.Footer>
-                            <CustomButton onClick={hideModal}>Close</CustomButton>
-                        </Modal.Footer>
-                    </Modal>
-                </div>
+                    <Modal.Footer>
+                        <CustomButton onClick={hideModal}>Close</CustomButton>
+                    </Modal.Footer>
+                </Modal>
             </div>
         </>
     );
